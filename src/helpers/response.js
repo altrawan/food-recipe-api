187f480b9420fa543808ba47a34fba54f91449bf
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-03-31 08:52:43 +0700

    Add REST API Response

--- src/helpers/response.js
+++ src/helpers/response.js
@@ -1,24 +1,27 @@
 module.exports = {
-  success: (res, status, message, data, pagination) => {
+  success: (res, code, status, message, data, pagination) => {
     const result = {};
-    result.status = status || 200;
+    result.code = code || 200
+    result.status = status || 'success';
     result.message = message;
     result.data = data;
     result.pagination = pagination;
-    return res.status(result.status).json(result);
+    return res.status(result.code).json(result);
   },
-  failed: (res, status, message, error) => {
+  failed: (res, code, status, message, error) => {
     const result = {};
-    result.status = status || 200;
+    result.code = code || 400;
+    result.status = status || 'failed';
     result.message = message;
     result.error = error;
-    return res.status(result.status).json(result);
+    return res.status(result.code).json(result);
   },
   successWithToken: (res, status, message, token) => {
-    res.json({
-      status,
-      message,
-      token
-    });
+    const result = {};
+    result.code = code || 200;
+    result.status = status || 'success';
+    result.message = message;
+    result.token = token;
+    return res.status(result.code).json(result);
   },
 };
