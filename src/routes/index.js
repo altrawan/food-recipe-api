151eab0e28eabc67fdc238c190be0bae1d07fab0
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-04-01 13:43:03 +0700

    Add Homepage

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const Router = express.Router();
 
+const homepageRoute = require('./homepage.route');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
 const recipeRoute = require('./recipe.route');
 const commentRoute = require('./comment.route');
-const likedRecipeRoute = require('./likedRecipeRoute');
-const savedRecipeRoute = require('./savedRecipeRoute');
+const likedRecipeRoute = require('./likedRecipe.route');
+const savedRecipeRoute = require('./savedRecipe.route');
 
+Router.use('/homepage', homepageRoute);
 Router.use('/auth', authRoute);
 Router.use('/user', userRoute);
 Router.use('/recipe', recipeRoute);
