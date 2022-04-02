39d2e15adcb561867db010260e145a2daa37bef7
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-04-02 14:03:40 +0700

    Add Redis Liked Recipe

---
+++ src/controllers/likedRecipe.controller.js
@@ -0,0 +1,150 @@
+const { v4: uuidv4 } = require('uuid');
+const likedRecipeModel = require('../models/likedRecipe.model');
+const { success, failed } = require('../helpers/response');
+const redis = require('../config/redis');
+
+module.exports = {
+  getAllLikedRecipe: async (req, res) => {
+    const { id } = req.params;
+    let { key, search, sort, sortType, page, limit } = req.query;
+    key = key || 'liked_recipes.id';
+    search = search ? `%${search}%` : '%';
+    sort = sort || 'liked_recipes.created_at';
+    sortType = sortType || 'DESC';
+    page = Number(page) || 1;
+    limit = Number(limit) || 3;
+
+    const offset = page * limit - limit;
+    const totalData = await likedRecipeModel.getCountLikedRecipe();
+    const totalPage = Math.ceil(totalData / limit);
+
+    const pageInfo = {
+      currentPage: page,
+      dataPerPage: limit,
+      totalPage,
+      totalData,
+    };
+
+    const result = await likedRecipeModel.getAllLikedRecipe(
+      key,
+      search,
+      sort,
+      sortType,
+      limit,
+      offset
+    );
+
+    if (result.rows.length < 1) {
+      return failed(res, 404, 'failed', 'Data not found');
+    }
+
+    if (page > totalPage) {
+      return failed(res, 400, 'failed', `Data only up to page ${totalPage}`);
+    }
+
+    redis.setEx(
+      `getLikedRecipe:${JSON.stringify(req.query)}`,
+      3600,
+      JSON.stringify({ result, pageInfo })
+    );
+
+    return success(
+      res,
+      200,
+      'success',
+      'Success get all data liked recipes',
+      result.rows,
+      pageInfo
+    );
+  },
+  getLikedRecipeById: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await likedRecipeModel.getLikedRecipeById(id);
+
+      if (result.rows.length < 1) {
+        return failed(res, 404, 'failed', `Data by id ${id} not found !`);
+      }
+
+      redis.setEx(`getLikedRecipe:${id}`, 3600, JSON.stringify(result));
+
+      return success(
+        res,
+        200,
+        'success',
+        `Success get liked recipe by id ${id}`,
+        result.rows[0]
+      );
+    } catch (error) {
+      return failed(res, 400, 'failed', `Bad Request : ${error.message}`);
+    }
+  },
+  getLikedRecipeByUser: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await likedRecipeModel.getLikedRecipeById(id);
+
+      if (result.rows.length < 1) {
+        return failed(res, 404, 'failed', `Data by id ${id} not found !`);
+      }
+
+      if (req.APP_DATA.tokenDecoded.id !== row.user_id) {
+        return failed(res, 403, 'failed', `You don't have access to this page`);
+      }
+
+      redis.setEx(`getLikedRecipeByUser:${id}`, 3600, JSON.stringify(result));
+    } catch (error) {
+      return failed(res, 400, 'failed', `Bad Request : ${error.message}`);
+    }
+  },
+  createLikedRecipe: async (req, res) => {
+    try {
+      let isNull;
+      const { user_id, recipe_id } = req.body;
+
+      const data = {
+        id: uuidv4(),
+        user_id,
+        recipe_id,
+      };
+
+      Object.keys(data).forEach((e) => {
+        if (!data[e]) isNull = e;
+      });
+
+      if (isNull) {
+        return failed(res, 400, 'failed', `${isNull} cannot be empty`);
+      }
+
+      const result = await likedRecipeModel.createLikedRecipe(data);
+      return success(
+        res,
+        200,
+        'success',
+        `Liked Recipe Added Successfully`,
+        result
+      );
+    } catch (error) {
+      return failed(res, 400, 'failed', `Bad Request : ${error.message}`);
+    }
+  },
+  deleteLikedRecipe: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const checkId = await likedRecipeModel.getLikedRecipeById(id);
+
+      if (checkId.rows.length < 1) {
+        return failed(res, 404, 'failed', `Data by id ${id} not found !`);
+      }
+
+      if (req.APP_DATA.tokenDecoded.id !== row.user_id) {
+        return failed(res, 403, 'failed', `You don't have access to this page`);
+      }
+
+      const result = await likedRecipeModel.deleteLikedRecipe(id);
+      return success(res, 200, 'success', `Liked Recipe Deleted Successfully`);
+    } catch (error) {
+      return failed(res, 400, 'failed', `Bad Request : ${error.message}`);
+    }
+  },
+};
---
+++ src/routes/likedRecipe.route.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const Router = express.Router();
+
+// Authentication
+const middlewareAuth = require('../middlewares/auth');
+// Redis
+const middlewareRedis = require('../middlewares/redis');
+// Controller
+const likedRecipeController = require('../controllers/likedRecipe.controller');
+
+Router.get(
+  '/',
+  middlewareAuth.authentication,
+  middlewareAuth.isAdmin,
+  middlewareRedis.getAllLikedRecipe,
+  likedRecipeController.getAllLikedRecipe
+)
+  .get(
+    '/user/:id',
+    middlewareAuth.authentication,
+    middlewareAuth.isUser,
+    middlewareRedis.getLikedRecipeById,
+    likedRecipeController.getLikedRecipeByUser
+  )
+  .get(
+    '/:id',
+    middlewareAuth.authentication,
+    middlewareRedis.getLikedRecipeByUser,
+    likedRecipeController.getLikedRecipeById
+  )
+  .post(
+    '/',
+    middlewareAuth.authentication,
+    middlewareRedis.clearLikedRecipe,
+    likedRecipeController.createLikedRecipe
+  )
+  .delete(
+    '/:id',
+    middlewareAuth.authentication,
+    middlewareRedis.clearLikedRecipe,
+    likedRecipeController.deleteLikedRecipe
+  );
+
+module.exports = Router;
