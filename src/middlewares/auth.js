c9a0f78e9df27d9d972a082292fd9fbe4cb7ae1e
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-04-02 19:34:06 +0700

    better roles users

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,30 +1,49 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../helpers/env');
 const { failed } = require('../helpers/response');
+const redis = require('../config/redis');
 
 module.exports = {
-  authentication: (req, res, next) => {
+  authentication: async (req, res, next) => {
     try {
       let token = req.headers.authorization;
-      token = token.split(" ")[1];
-      
+
       if (!token) {
         return failed(res, 403, 'failed', 'Please login first');
       }
 
+      token = token.split(' ')[1];
+      
+      const result = await redis.get(`accessToken:${token}`);
+      if (result) {
+        return failed(
+          res,
+          403,
+          'failed',
+          `Your token is destroyed please login again`
+        );
+      }
+
       const decoded = jwt.verify(token, JWT_SECRET);
       req.APP_DATA = {
-        tokenDecoded: decoded
+        tokenDecoded: decoded,
       };
-      
+
       return next();
     } catch (error) {
-      return failed(res, 400, 'failed', 'Invalid token', error)
+      return failed(res, 400, 'failed', 'Invalid token', error);
     }
   },
   isAdmin: (req, res, next) => {
     const token = req.APP_DATA.tokenDecoded;
     if (token.level === 0) {
+      return next();
+    }
+    return failed(res, 403, 'failed', `You don't have access to this page`);
+  },
+  isUser: (req, res, next) => {
+    const token = req.APP_DATA.tokenDecoded;
+    if (token.level === 1) {
       return next();
     }
     return failed(res, 403, 'failed', `You don't have access to this page`);
