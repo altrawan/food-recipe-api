294b4164ab2ab676b75438a88cc156668ea3064f
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-04-03 12:21:46 +0700

    Fix homepage endpoints

--- src/models/homepage.model.js
+++ src/models/homepage.model.js
@@ -4,7 +4,10 @@ module.exports = {
   getLatestRecipe: (limit) =>
     new Promise((resolve, reject) => {
       db.query(
-        `SELECT * FROM recipes ORDER BY created_at DESC LIMIT $1`,
+        `SELECT recipes.title, recipes.image, users.name, 
+        to_char(recipes.created_at, 'FMDay, DD FMMonth YYYY HH24:MI:SS') AS date 
+        FROM recipes INNER JOIN users ON recipes.user_id = users.id 
+        ORDER BY recipes.created_at DESC LIMIT $1`,
         [limit],
         (err, res) => {
           if (err) {
