17e69665b66334bae8091423e82a42a0d2597c88
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-04-03 12:15:40 +0700

    Fixed Environment

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -3,9 +3,16 @@ require('dotenv').config();
 // export environment
 module.exports = {
   PORT: process.env.PORT,
+
   PGHOST: process.env.PGHOST,
   PGUSER: process.env.PGUSER,
   PGDATABASE: process.env.PGDATABASE,
   PGPASSWORD: process.env.PGPASSWORD,
   PGPORT: process.env.PGPORT,
+
+  JWT_SECRET: process.env.JWT_SECRET,
+
+  STMP_SERVICE: process.env.STMP_SERVICE,
+  STMP_USER: process.env.STMP_USER,
+  STMP_PASS: process.env.STMP_PASS,
 };
