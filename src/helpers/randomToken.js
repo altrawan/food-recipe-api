f63e05efe96111a9f92ac05ff299bef20276ccc3
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-04-03 18:18:13 +0700

    Create random token

---
+++ src/helpers/randomToken.js
@@ -0,0 +1,8 @@
+const characters =
+  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let token = '';
+for (let i = 0; i < 25; i++) {
+  token += characters[Math.floor(Math.random() * characters.length)];
+}
+
+module.exports = token;
