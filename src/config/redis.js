9411b440eec759f6a5d52168527cf3f14f3135fe
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-04-15 14:38:49 +0700

    Fixed Typo

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,14 +1,21 @@
 const redis = require('redis');
 const { RDS_HOST, RDS_PORT, RDS_PASS } = require('../helpers/env');
 
-const client = redis.createClient({
-  host: RDS_HOST,
-  port: RDS_PORT,
-  password: RDS_PASS,
-});
+let client;
+if (RDS_HOST) {
+  let redisURL = url.parse(RDS_HOST);
+  client = redis.createClient(redisURL);
+} else {
+  client = redis.createClient();
+}
+// const client = redis.createClient({
+//   host: RDS_HOST,
+//   port: RDS_PORT,
+//   password: RDS_PASS,
+// });
 
-client.on('error', (err) => console.log(`Redis Client Error : ${err}`));
-client.on('connect', () => console.log(`You're now connected db redis`));
+// client.on('error', (err) => console.log(`Redis Client Error : ${err}`));
+// client.on('connect', () => console.log(`You're now connected db redis`));
 
 // client.connect();
 
