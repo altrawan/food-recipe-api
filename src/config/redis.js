75951cf584421a0270d72f1a907cae9a5b53cba2
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-04-15 14:15:57 +0700

    Fixed Typo

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,6 +1,11 @@
 const redis = require('redis');
+const { RDS_HOST, RDS_PORT, RDS_PASS } = require('../helpers/env');
 
-const client = redis.createClient();
+const client = redis.createClient({
+  host: RDS_HOST,
+  port: RDS_PORT,
+  password: RDS_PASS,
+});
 
 client.on('error', (err) => console.log(`Redis Client Error : ${err}`));
 // client.on('connect', () => console.log(`You're now connected db redis`));
--- src/helpers/env.js
+++ src/helpers/env.js
@@ -15,4 +15,8 @@ module.exports = {
   STMP_SERVICE: process.env.STMP_SERVICE,
   STMP_USER: process.env.STMP_USER,
   STMP_PASS: process.env.STMP_PASS,
+
+  RDS_HOST: process.env.RDS_HOST,
+  RDS_PORT: process.env.RDS_PORT,
+  RDS_PASS: process.env.RDS_PASS,
 };
